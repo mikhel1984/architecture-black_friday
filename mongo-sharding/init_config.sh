a5d10805805387a63773ca1fc77d3a2b61e92be5
Stanislav <mikhel.sk@gmail.com>
2025-08-19 20:41:59 +0300

    Configure sharding

--- mongo-sharding/init_config.sh
+++ mongo-sharding/init_config.sh
@@ -1,4 +1,40 @@
-docker compose exec -T shadr1 mongosh --port 27018 --quiet <<EOF
-use somedb
-db.helloDoc.countDocuments()
+echo configuration service
+docker compose exec -T config_srv mongosh --port 27017 --quiet <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+    configsvr: true,
+    members: [
+      { _id : 0, host : "config_srv:27017" }
+    ]
+  }
+);
+exit();
 EOF
+
+echo shard 1
+docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1:27018" },
+      ]
+    }
+);
+exit();
+EOF
+
+echo shard 2
+docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+        { _id : 0, host : "shard2:27019" },
+      ]
+    }
+);
+exit();
+EOF
+
